f471c0e6f1d8e7a4fe58dcb7897d4a992d98854a
Abhijith ps <abhijithabi22102001@gmail.com>
2023-08-17 19:34:27 +0530

    updated api calls

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var axios = require("axios");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", async function (req, res, next) {
+  try {
+    const response = await axios.get(
+      "https://securtity-website.azurewebsites.net/api/v1/our-service"
+    );
+    const data = response.data; // Extract the data from the response
 
+    // Log the API response to the console
+    console.log("API Response:", data);
+
+    res.render("index", { title: "Express", data: data }); // Pass the data to the template
+  } catch (error) {
+    console.error("Error fetching API data:", error);
+    next(error); // Pass the error to Express error handler
+  }
+});
 module.exports = router;
